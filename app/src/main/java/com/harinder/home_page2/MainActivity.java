1f28cddbf779d9039752b96c676b240adcde3684
Harinder Pal Singh <harinder2612@gmail.com>
2017-02-02 18:37:06 +0530

    Initial commit

---
+++ app/src/main/java/com/harinder/home_page2/MainActivity.java
@@ -0,0 +1,26 @@
+package com.harinder.home_page2;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Color;
+import android.graphics.drawable.ColorDrawable;
+import android.support.v7.app.ActionBar;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+
+public class MainActivity extends Activity {
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+    }
+public void home(View view)
+ {
+   Intent i= new Intent(this,second_act.class);
+     startActivity(i);
+ }
+}
---
+++ app/src/main/java/com/harinder/home_page2/second_act.java
@@ -0,0 +1,14 @@
+package com.harinder.home_page2;
+
+import android.app.Activity;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class second_act extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_second_act);
+    }
+}
